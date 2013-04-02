2ab0a0936f7ddb5a10ab6ffbc8326db58dcd61fa
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-04-02 14:17:37 +0000

    Change the compare to match the listing according to the evaluation about most significant.

--- app/controllers/XMLPeriodComparator.java
+++ app/controllers/XMLPeriodComparator.java
@@ -40,7 +40,7 @@ public class XMLPeriodComparator implements Comparator {
 		this.base = base;
 	}
 
-	public int compare(Object a, Object b) {
+	public int compare1(Object a, Object b) {
 
 		if( ((XMLPeriod) a).getHoursIDREF() < ((XMLPeriod) b).getHoursIDREF()) {
 			return -1;
@@ -49,6 +49,45 @@ public class XMLPeriodComparator implements Comparator {
 		} else {
 			return 1;
 		}
+	}
+
+	public int compare(Object a, Object b) {
+		boolean amonth = ((XMLPeriod) a).getMonths().isEmpty();
+		System.out.println("amonth " + amonth);
+		boolean bmonth = ((XMLPeriod) b).getMonths().isEmpty();
+		System.out.println("bmonth " + bmonth);
+		boolean aweek = ((XMLPeriod) a).getWeeks().isEmpty();
+		System.out.println("aweek " + aweek);
+		boolean bweek = ((XMLPeriod) b).getWeeks().isEmpty();
+		System.out.println("bweek " + bweek);
+		
+		if (amonth && aweek) 
+			return 1;
+		
+		if (amonth && !bmonth)
+			return 1;
+		
+		if (aweek && !bweek)
+			return 1;
+
+		if (bmonth && bweek) 
+			return -1;
+		
+		if (bmonth && !amonth)
+			return -1;
+		
+		if (bweek && !aweek)
+			return 1;
+
+		return 0;
+		/*
+		if( ((XMLPeriod) a).getHoursIDREF() < ((XMLPeriod) b).getHoursIDREF()) {
+			return -1;
+		} else if(((XMLPeriod) a).getHoursIDREF() == ((XMLPeriod) b).getHoursIDREF()) {
+			return 0;
+		} else {
+			return 1;
+		}*/
 	}
 
 }
