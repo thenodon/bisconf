105fb453d95b0937fb6f816423c1127e2ba09fe6
aha <aha@7d451ef3-0636-4db7-8f56-89c34b65e6bc>
2013-10-27 17:56:45 +0000

    Showing newest version first

--- app/models/ConfigMeta.java
+++ app/models/ConfigMeta.java
@@ -9,22 +9,17 @@ public class ConfigMeta implements Comparable<ConfigMeta>{
 	public String directory;
 	public boolean current = false;
 	
-	
+	/*
+	 * Showing newest version first.
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	@Override
 	public int compareTo(ConfigMeta o) {
-		if (Long.parseLong(this.directory) < Long.parseLong(o.directory)) return -1;
-		if (Long.parseLong(this.directory) > Long.parseLong(o.directory)) return 1;
+		if (Long.parseLong(this.directory) < Long.parseLong(o.directory)) return 1;
+		if (Long.parseLong(this.directory) > Long.parseLong(o.directory)) return -1;
 		
 		return 0;
 	}
-/*	@Override
-	public int compareTo(ConfigMeta o) {
-		if (Long.parseLong(this.directory.substring(this.directory.lastIndexOf(File.separator)+1) ) < 
-				Long.parseLong(o.directory.substring(o.directory.lastIndexOf(File.separator)+1))) return -1;
-		if (Long.parseLong(this.directory.substring(this.directory.lastIndexOf(File.separator)+1) ) > 
-				Long.parseLong(o.directory.substring(o.directory.lastIndexOf(File.separator)+1))) return 1;
-		
-		return 0;
-	}
-*/
+	
+
 }
